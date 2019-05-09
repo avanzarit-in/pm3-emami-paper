3736af696720b4d6dff29457343f907366636c3f
Netranand Mehar <netranandm1@gmail.com>
2019-05-09 12:49:48 +0530

    .

--- src/components/data-grid/DateEditor.js
+++ src/components/data-grid/DateEditor.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import moment from 'moment'
+import { Form, Grid, Input, Button, Icon, Modal } from 'semantic-ui-react';
 import MonthCalendar from '../calendar/monthCalendar/MonthCalendar';
+export default class DataGridToolBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.state.showModal = false;
 
-
-export default class DateEditor extends Component {
-
-
+    }
+    close = () => {
+        this.setState({ showModal: false })
+    }
+    setDate = (e, date) => {
+        console.log("date String =>" + date.format("YYYY/MM/DD"));
+        this.close();
+    }
     render() {
         return (
+            <Grid padded>
+              
+                        
+                <Modal open={this.state.showModal} size='tiny' closeOnEscape={true}
+                    closeOnDimmerClick={true} centered={false} onClose={this.close}>
+                    <Modal.Header>Select Date</Modal.Header>
+                    <Modal.Content >
+                        <MonthCalendar date={moment()} callback={this.setDate} />
+                    </Modal.Content>
 
-                    <MonthCalendar/>
-               
+                </Modal>
+            </Grid>
 
-          
         );
     }
 }
--- src/components/parent-reel-management/ParentReelManagement.js
+++ src/components/parent-reel-management/ParentReelManagement.js
@@ -4,13 +4,14 @@ import withDataServices from './../hoc/withDataServices';
 import { Dimmer, Loader } from 'semantic-ui-react';
 import DateEditor from './../data-grid/DateEditor';
 import DateFormatter from './../data-grid/DateFormatter';
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
 
 
 const columns = [
     { key: "LOTNO", title: "LOT NO", mandatory: true },
     { key: "PARENTRLNO", title: "PARENT REEL NO", mandatory: true },
-    { key: "MFGDATE", title: "REEL MFG DATE", mandatory: false, editor : DateEditor,formatter:DateFormatter },
+    { key: "MFGDATE", title: "REEL MFG DATE", mandatory: false, formatter: DateFormatter },
     { key: "WEIGHT", title: "PARENT ROLL WT", mandatory: true }
 ];
 
@@ -31,26 +32,26 @@ class ParentReelManagement extends Component {
         })
     }
 
-     onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
-    this.setState(state => {
-      const rows = state.rows.slice();
-      for (let i = fromRow; i <= toRow; i++) {
-        rows[i] = { ...rows[i], ...updated };
-      }
-      return { rows };
-    });
-  };
+    onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+        this.setState(state => {
+            const rows = state.rows.slice();
+            for (let i = fromRow; i <= toRow; i++) {
+                rows[i] = { ...rows[i], ...updated };
+            }
+            return { rows };
+        });
+    };
     saveHook = (item) => {
         console.log(item);
         return new Promise((resolve, reject) => {
-            if ( item.PARENTRLNO === "" || item.MFGDATE === "" || item.WEIGHT === "") {
+            if (item.PARENTRLNO === "" || item.MFGDATE === "" || item.WEIGHT === "") {
                 resolve(false);
             } else {
-                this.props.parentReelMasterService.patch(item.ROWID, 
-                { "LOTNO": item.LOTNO, "PARENTRLNO": item.PARENTRLNO, "MFGDATE": item.MFGDATE, "WEIGHT": item.WEIGHT })
-                .then(patchedItem => {
-                    resolve(true);
-                })
+                this.props.parentReelMasterService.patch(item.ROWID,
+                    { "LOTNO": item.LOTNO, "PARENTRLNO": item.PARENTRLNO, "MFGDATE": item.MFGDATE, "WEIGHT": item.WEIGHT })
+                    .then(patchedItem => {
+                        resolve(true);
+                    })
             }
         })
     }
@@ -62,17 +63,19 @@ class ParentReelManagement extends Component {
     }
 
     render() {
+
         return (
+
             !this.state.isLoading ? <
-            DataGrid columns={columns} minHeight={467} createNewRowHandler={this.createNewRow} rows={this.state.rows} saveHook={(item) => this.saveHook(item)} 
-            onGridRowsUpdated={this.onGridRowsUpdated}
-            enableCellSelect={true}
+                DataGrid columns={columns} minHeight={467} createNewRowHandler={this.createNewRow} rows={this.state.rows} saveHook={(item) => this.saveHook(item)}
+                onGridRowsUpdated={this.onGridRowsUpdated}
+                enableCellSelect={true}
             /> :
-            <Dimmer active inverted style={{ position: 'fixed', top: "50%" }}>
-                <Loader size='large'>Loading</Loader>
-            </Dimmer>
-           
-               
+                <Dimmer active inverted style={{ position: 'fixed', top: "50%" }}>
+                    <Loader size='large'>Loading</Loader>
+                </Dimmer>
+
+
         );
     }
 }
